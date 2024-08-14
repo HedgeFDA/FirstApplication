ae70837f3931c6a0d88d9aabc6a929d00501e34e
Ilya Boiko <boiko.ilya.evg@gmail.com>
2024-08-14 10:06:57 +0500

    Добавьте файлы проекта.

---
+++ FirstApplication.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; // Подключенное пространство имен
+
+namespace FirstApplication.ConsoleApp // Объявление пространства имен для данного кода
+{
+    class Program // Объявление класса Program
+    {
+        static void Main(string[] args) // Объявление метода Main
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Здравствуйте, {name}, сколько будет 2+2*2?");
+            string answer = Console.ReadLine();
+            if (answer == "6")
+            {
+                Console.WriteLine("Угадали, будет {0} ", answer);
+            }
+            else
+            {
+                Console.WriteLine("Не угадали, будет не {0}", answer);
+            }
+        }
+    }
+}
